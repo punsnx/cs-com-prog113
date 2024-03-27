408841a12a1418a34420b67a8d595d6101438a10
Sirisuk Tharntham <79169700+punsnx@users.noreply.github.com>
2024-03-28 04:02:23 +0700

    implement linklist class with addNodeSort() deleteNode() all member and operator overloaded

--- Lab/final/classLinklist.cc
+++ Lab/final/classLinklist.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <cstdlib>
+#include <memory>
 using namespace std;
 
 class Node {
@@ -41,6 +42,61 @@ public:
         cur->setNext(new_node);
         return *this;
     }
+    Linklist &addNodeSort(int data){
+        Node *cur = head,*prev=NULL;
+        Node *new_node = new Node(data,NULL);
+        if(cur == NULL){
+            head = new_node;
+            return *this;
+        }
+        while(cur && cur->getData() <= data){
+            prev = cur;
+            cur = cur->getNext();
+        }
+        if(prev == NULL){
+            new_node->setNext(head);
+            head = new_node;
+            return *this;
+        }
+        new_node->setNext(cur);
+        prev->setNext(new_node);
+        return *this;
+    }
+    Linklist &deleteNode(int data){
+        Node *cur=head,*prev=NULL;
+        while(cur && cur->getData() != data){
+            prev = cur;
+            cur = cur->getNext();
+        }
+        if(cur == NULL){return *this;}
+        if(prev == NULL){
+            head = cur->getNext();
+            delete(cur);
+            return *this;
+        }
+        prev->setNext(cur->getNext());
+        delete(cur);
+        return *this;
+    }
+    Linklist &deleteNodeR(int data){
+        Node *cur = head,*prev=NULL;
+        while(cur && cur->getData() == data){
+            head = cur->getNext();
+            delete(cur);
+            cur = head;
+        }
+        while(cur){
+            if(cur->getData() == data){
+                prev->setNext(cur->getNext());
+                delete(cur);
+                cur = prev->getNext();
+            }else{                
+                prev = cur;
+                cur = cur->getNext();
+            }
+        }
+        return *this;
+    }
     void print(){
         Node *cur = head;
         stringstream ss;
@@ -52,16 +108,45 @@ public:
         ss << "]";
         cout << ss.str() << endl;
     }
-
-
+    Node &operator[](int n){
+        Node *cur = head;
+        for(int i = 0;i<n;i++){
+            cur = cur->getNext();
+        }
+        return *cur;
+    }
+    Linklist &operator+(Linklist &list2){
+        Node * cur = head;
+        if (cur == NULL){
+            head = &list2[0];
+            return *this;
+        }
+        while(cur->getNext() != NULL){
+            cur = cur->getNext();
+        }
+        cur->setNext(&list2[0]);
+        return *this;
+    }
 };
-
-
-
 int main(){
-    Node aa;
-    Linklist a(&aa);
-    a.addNode(0).addNode(1);
+    // Node aa;
+    Linklist a,b,c;
+    a.addNodeSort(5);
+    a.addNodeSort(1);
+    a.addNodeSort(3).addNodeSort(5);
+    b.addNodeSort(1).addNodeSort(1);
+    c.addNodeSort(2);
+    // a.deleteNode(3).deleteNode(5);
     a.print();
+    b.print();
+    c.print();
+    a + b + c;
+    a.print();
+    cout << "a[1] = " << a[1].getData() << endl;
+    a.deleteNodeR(5).deleteNodeR(1);
+    a.print();
+    cout << "a[1] = " << a[1].getData() << endl;
+
+    
 
 }
