71581cab75df4e8622a431956b46f4d3901914d2
Sirisuk Tharntham <79169700+punsnx@users.noreply.github.com>
2024-03-27 23:42:52 +0700

    implement class node & linklist with addNode() self return ref and  print() member function

--- Lab/final/classLinklist.cc
+++ Lab/final/classLinklist.cc
@@ -1,7 +1,67 @@
 #include <iostream>
+#include <sstream>
+#include <cstdlib>
 using namespace std;
 
-int main(){
-
+class Node {
+private:
+    int data;
+    Node* next;
+public:
+    Node():data(0),next(NULL){}
+    Node(int d):data(d),next(NULL){}
+    Node(int d,Node *n):data(d),next(n){}
     
+    int getData(){return data;}
+    Node* getNext(){return next;}
+
+    void setData(int d){
+        data = d;
+    }
+    void setNext(Node *n){
+        next = n;
+    }
+};
+class Linklist{
+private:
+    Node *head;
+public:
+    Linklist():head(NULL){}
+    Linklist(Node *ref):head(ref){}
+    Linklist &addNode(int data){
+        Node *cur = head;
+        Node *new_node = new Node(data,NULL);
+        if (cur == NULL){
+            head = new_node;
+            return *this;
+        }
+        while(cur && cur->getNext() != NULL){
+            cur = cur->getNext();
+        }
+        cur->setNext(new_node);
+        return *this;
+    }
+    void print(){
+        Node *cur = head;
+        stringstream ss;
+        ss << "[ ";
+        while(cur){
+            ss << cur->getData() << " ";
+            cur = cur->getNext();
+        }
+        ss << "]";
+        cout << ss.str() << endl;
+    }
+
+
+};
+
+
+
+int main(){
+    Node aa;
+    Linklist a(&aa);
+    a.addNode(0).addNode(1);
+    a.print();
+
 }
