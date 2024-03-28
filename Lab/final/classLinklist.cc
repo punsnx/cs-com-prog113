db844c97a387f1b7b97d0ba9b7a9a58ea3d74e34
Sirisuk Tharntham <79169700+punsnx@users.noreply.github.com>
2024-03-29 00:05:00 +0700

    just try to practice handling

--- Lab/final/classLinklist.cc
+++ Lab/final/classLinklist.cc
@@ -29,6 +29,14 @@ private:
 public:
     Linklist():head(NULL){}
     Linklist(Node *ref):head(ref){}
+    ~Linklist(){
+        Node *cur=head,*prev=NULL;
+        while(cur){
+            prev = cur;
+            cur = cur->getNext();
+            delete prev;
+        }
+    }
     Linklist &addNode(int data){
         Node *cur = head;
         Node *new_node = new Node(data,NULL);
@@ -128,22 +136,29 @@ public:
         return *this;
     }
 };
-// int main(){
-//     // Node aa;
-//     Linklist a,b,c;
-//     a.addNodeSort(5);
-//     a.addNodeSort(1);
-//     a.addNodeSort(3).addNodeSort(5);
-//     b.addNodeSort(1).addNodeSort(1);
-//     c.addNodeSort(2);
-//     // a.deleteNode(3).deleteNode(5);
-//     a.print();
-//     b.print();
-//     c.print();
-//     a + b + c;
-//     a.print();
-//     cout << "a[1] = " << a[1].getData() << endl;
-//     a.deleteNodeR(5).deleteNodeR(1);
-//     a.print();
-//     cout << "a[1] = " << a[1].getData() << endl;
-// }
+int main(){
+    // Node aa;
+    Linklist 
+    *a = new Linklist(),
+    *b = new Linklist(),
+    *c = new Linklist();
+
+    a->addNodeSort(5);
+    a->addNodeSort(1);
+    a->addNodeSort(3).addNodeSort(5);
+    b->addNodeSort(1).addNodeSort(1);
+    c->addNodeSort(2);
+    // a.deleteNode(3).deleteNode(5);
+    a->print();
+    b->print();
+    c->print();
+    *a + *b + *c;
+    a->print();
+    cout << "a[1] = " << (*a)[1].getData() << endl;
+    a->deleteNodeR(5).deleteNodeR(1);
+    a->print();
+    cout << "a[1] = " << (*a)[1].getData() << endl;
+    (*a)[1].setData(20);
+    cout << "a[1] = " << (*a)[1].getData() << endl;
+    delete a,b,c;
+}
--- Lab/final/demo.c
+++ Lab/final/demo.c
@@ -17,8 +17,22 @@ typedef struct _demo
 
 int main()
 {
-    number A;
-    A.a = 65;
-    demo B = {1, 0};
-    printf("%lu", sizeof(B));
+
+    // number A;
+    // A.a = 65;
+    // demo B = {1, 0};
+    // printf("%lu", sizeof(B));
+
+    char str[5];
+    sprintf(str,"%04d",10);
+    int n = 11;
+    for(int i = 0;i<n;i++){
+        printf("%d ",(int)str[i]);
+    }
+    printf("\n");
+    for(int i = 0;i<n;i++){
+        printf("%2c ",str[i]);
+    }
+    printf("\n");
+    printf("%s",str);
 }
